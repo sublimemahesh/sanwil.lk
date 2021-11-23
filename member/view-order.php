f371be214bafcce1b9bbdd1e5e4cfd9f7d06abf9
mahesh@synotec.lk <mahesh@synotec.lk>
2021-11-23 11:00:37 +0530

    #21 Member section change

--- member/view-order.php
+++ member/view-order.php
@@ -23,7 +23,7 @@ $OCITY = new City($ORDER->city);
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-    <title>Sanwil.lk</title>
+    <title>Sanwil Products</title>
 
 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
